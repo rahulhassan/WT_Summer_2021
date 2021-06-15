8b53855d17155a868da6be51a55decc53e3a3ceb
Rahul <specialistrahul@gmail.com>
2021-06-15 21:22:16 +0200

    task added

--- LabTask4/myform.php
+++ LabTask4/myform.php
@@ -1,4 +1,8 @@
-<?php include "control/process.php"; ?>
+<?php 
+	include "control/process.php";
+	include "control/results.php";
+
+?>
 <!DOCTYPE html>
 <html>
 	<head>
@@ -9,26 +13,26 @@
 		
 		<hr>
 		
-		<form action="" method="post">
+		<form action="" method="post" enctype="multipart/form-data">
 			<table>
 				<tr>
 				<td>Full Name :</td>
-				<td><input name="fname" type="text"></td>
+				<td><input name="fname" type="text"> <?php echo $validatename; ?></td>
 				</tr>
 				
 				<tr>
 				<td>Email :</td>
-				<td><input type="email" name="email"> </td>
+				<td><input type="email" name="email"> <?php echo $validateemail; ?></td>
 				</tr>
 				
 				<tr>
 				<td>Password :</td>
-				<td><input type="password" name="password"> </td>
+				<td><input type="password" name="password"> <?php echo $validatepass ?></td>
 				</tr>
 				
 				<tr>
 				<td>Comment :</td>
-				<td><textarea rows="4" cols="40" name="comment"></textarea></td></td>
+				<td><textarea rows="4" cols="40" name="comment"></textarea> <?php echo $validatecmt; ?></td></td>
 				</tr>
 				
 				<tr>
@@ -39,12 +43,38 @@
 						<input type="radio" id="other" name="gender" value="Other"> Other
 					</td>
 				</tr>
+                <tr>
+                    <td>
+                        <?php echo $infog;?>
+                    </td>
+					<td>
+                        <?php echo $validateradio; ?>
+					</td>
+				</tr>
+			   
 				<tr>
 					<td>Please Choose a hobby :</td>
 					<td>
 						<input type="checkbox" name= "hobby1" value="Singing"> Singing
 						<input type="checkbox" name= "hobby2" value="Dancing"> Dancing
 						<input type="checkbox" name= "hobby3" value="Reading"> Reading
+					</td>
+				</tr>
+                <tr>
+                    <td>
+                        <?php echo $infoh;?>
+                    </td>
+                    <td>
+                        <?php echo $validatecheckbox; ?>
+                        <?php echo $h1;?>
+                        <?php echo $h2;?>
+                        <?php echo $h3;?>
+    
+                    </td>
+				</tr>
+					<td>Please Choose a file :</td>
+					<td>
+						<input type="file" name="filetoupload">
 					</td>
 				</tr>
 				<tr>
